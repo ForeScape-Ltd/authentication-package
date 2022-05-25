5dfc264be574e7475185798631c015dabc57965c
FG <felixgiftinfo@gmail.com>
2022-05-25 15:21:56 +0100

    Modified index.go

--- index.go
+++ index.go
@@ -9,10 +9,6 @@ import (
 	"github.com/felixgiftinfo/authentication-pkg/token"
 )
 
-// type AuthenticationContext struct {
-// 	TokenMaker token.Maker
-// }
-
 func TODO(tokenSecretKey, refreshTokenSecretKey string, tokenDuration, refreshTokenDuration time.Duration) (token.Maker, error) {
 
 	jwt, err := token.NewJWTMaker(tokenSecretKey, refreshTokenSecretKey, tokenDuration, refreshTokenDuration)
